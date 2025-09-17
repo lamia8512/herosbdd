db360ed9b64b48f907f6a5b211ee54046e9eb1dd
lamia 8512 <elf.lamia@hotmail.fr>
2025-09-17 14:02:06 +0200

    mise a jour

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -9,16 +9,21 @@
     <link rel="stylesheet" href="/public/css/style.css">
 </head>
 <body>
-    <header>
-        <div class="contener">
-            <h1>Héros BDD</h1>
-           
-            <nav>
-                <ul>
-                    <li><a href="accueil">ACCUEIL</a></li>
-                    <li><a href="404.php">404</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Heros Projet</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/register">S'inscrire</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Se connecter</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
