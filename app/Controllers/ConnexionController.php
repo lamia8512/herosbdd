eaad8be50900e640a7080a9b36ca7b1abec98642
lamia 8512 <elf.lamia@hotmail.fr>
2025-09-17 11:59:41 +0200

    fonction redirecttoroute

--- app/Controllers/ConnexionController.php
+++ app/Controllers/ConnexionController.php
@@ -5,13 +5,10 @@ if(isset($_POST['pseudo'])){
 
     $valueEmail = htmlspecialchars($_POST['email']);
     $valuePassword = htmlspecialchars($_POST['password']);
-    $valuePseudo = htmlspecialchars($_POST['pseudo']);
 
-    checkFormat('pseudo', $valuePseudo);
     checkFormat('password', $valuePassword);
     checkFormat('email', $valueEmail);
 
-    isNotEmpty('pseudo');
     isNotEmpty('email');
     isNotEmpty('password');
     
@@ -26,26 +23,23 @@ if(isset($_POST['pseudo'])){
 
         // 0- Je creer la requête SQL:
         $query = "INSERT INTO `user` (`pseudo`, `password`, `email`) 
-        VALUES (:pseudo, :password, :email)";
+        VALUES (:password, :email)";
 
         // 1- prépare la requête :
         $queryStatement = $pdo->prepare($query);
 
         // 2- lier les marqueurs aux valeurs :
-        $queryStatement->bindValue(':pseudo', $valuePseudo);
         $queryStatement->bindValue(':password', $passwordHash);
         $queryStatement->bindValue(':email', $valueEmail);
 
         // 3- exécuter la requête :
         $queryStatement->execute();
 
-        redirectToRoute('/');
     }
 
-    require_once( __DIR__ . "/../Views/register.view.php" );
+    require_once( __DIR__ . "/../Views/connexion.view.php" );
 }else{
 
-    require_once( __DIR__ . "/../Views/register.view.php" );
+    require_once( __DIR__ . "/../Views/connexion.view.php" );
 }
-    
     
